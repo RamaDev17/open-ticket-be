888aab49d1b69b90dfc70b23b07ee747e2d33ef7
RamaDev17 <ramdhaney.dev17@gmail.com>
2025-06-01 18:51:43 +0700

    feat: create migration and model User, Ticket and TicketReplies

---
+++ database/migrations/2025_06_01_073738_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('code');
+            $table->string('title');
+            $table->longText('description');
+            $table->enum('status', ['open', 'in_progress', 'resolved', 'rejected'])->default('open');
+            $table->enum('priority', ['low', 'medium', 'high'])->default('low');
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
